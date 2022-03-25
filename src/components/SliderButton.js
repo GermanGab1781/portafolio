21a7d4bd12e28d2e1c76031c86d3e3ed229e7970
unknown <germangabriel1998@gmail.com>
2022-03-25 17:13:57 -0300

    Sixth commit (refined slider with more animations and props)

--- src/components/SliderButton.js
+++ src/components/SliderButton.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
-import { Button } from "react-bootstrap";
 import "../slider.css";
 
-export default function SliderButton() {
+export default function SliderButton(props) {
   
   const [show, setShow]= useState(false);
   
   return (
-    <div className={show ? "sliderInfo show" : "sliderInfo"}>
-        <Button onClick={()=>setShow(!show)}>GO</Button>
+    <div className={show ? "sliderInfo show" : "sliderInfo"}>     
+      <div onClick={()=> setShow(!show)} className="buttonPrefs">
+        <span className={show ? "arrow down" : "arrow up"}></span>
+      </div>
+      <div className={show ? "sliderChildren show" : "sliderChildren"}>
+        {props.children}
+      </div>           
     </div>
   )
 }
